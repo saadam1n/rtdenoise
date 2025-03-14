776907833feeb778c0efdbff68377590cf6d871b
Saad Amin <58188800+saadam1n@users.noreply.github.com>
2025-03-14 13:32:31 -0400

    Allow support for multiple models to be trained simulatenously

--- scripts/sample_train.py
+++ scripts/sample_train.py
@@ -8,18 +8,6 @@ import openexr_numpy as exr
 
 import os
 
-def unsqueeze_inputs(data):
-    inputs, reference = data
-
-    if inputs.dim() < 4:
-        inputs = inputs.unsqueeze(0)
-        reference = reference.unsqueeze(0)
-    elif inputs.dim() > 4:
-        inputs = inputs.flatten(start_dim=0, end_dim=1)
-        reference = reference.flatten(start_dim=0, end_dim=1)
-
-    return inputs, reference
-
 # comment to force recration of docker image
 # please force recreation
 if __name__ == "__main__":
@@ -31,61 +19,59 @@ if __name__ == "__main__":
     else:
         raise RuntimeError("Unable to find suitable GPU for training!")
 
-    dataset = rtdenoise.PrebatchedDataset(os.environ['RTDENOISE_DATASET_PATH'], buffers=["color", "albedo", "normal", "motionvec"], truncated_batch_size=16)
+    dataset = rtdenoise.PrebatchedDataset(os.environ['RTDENOISE_DATASET_PATH'], buffers=["color", "albedo", "normal", "motionvec"], truncated_batch_size=2)
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=True, num_workers=2, prefetch_factor=2)
 
-    model = torch.nn.DataParallel(rtdenoise.LaplacianPyramidUNet().to(device))
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.005)
-    scheduler  = torch.optim.lr_scheduler.StepLR(optimizer, step_size=4, gamma=0.85)
+    models = [
+        rtdenoise.LaplacianPyramidUNet(),
+        rtdenoise.SingleFrameDiffusion()
+    ]
 
-    model, losses = rtdenoise.train_model(dataset, dataloader, model=model, optimizer=optimizer, scheduler=scheduler, num_epochs=32, device=device)
+    names = [
+        type(model).__name__ for model in models
+    ]
+
+    parallel_models = [
+        torch.nn.DataParallel(model.to(device)) for model in models
+    ]
+
+    optimizers = [
+        torch.optim.Adam(model.parameters()) for model in parallel_models
+    ]
+
+    schedulers = [
+        torch.optim.lr_scheduler.StepLR(optimizer, step_size=4, gamma=0.85) for optimizer in optimizers
+    ]
+
+    model, losses = rtdenoise.train_model(dataset, dataloader, models=parallel_models, optimizers=optimizers, schedulers=schedulers, names=names, num_epochs=1, device=device)
 
     print("Losses over time:")
-    f = open(f"{os.environ['RTDENOISE_OUTPUT_PATH']}/latest-losses.csv", "w")
-    f.write("Epoch, Training Loss, Eval Loss\n")
-    for i, loss in enumerate(losses):
-        train, eval = loss
+    with open(f"{os.environ['RTDENOISE_OUTPUT_PATH']}/latest-losses.csv", "w") as f:
+        header = "\tEpoch Index"
+        for model in models:
+            header = f"{header}, {type(model).__name__} training loss"
+        for model in models:
+            header = f"{header}, {type(model).__name__} eval loss"
+        
+        print(header)
+        f.write(f"{header}\n")
+    
+        for i, loss in enumerate(losses):
+            csv_row = f"{i}"
+            console_row = f"\tEpoch {i}:\t"
 
-        print(f"\tEpoch {i}:\t{train}\t{eval}")
-        f.write(f"{i}, {train}, {eval}\n")
+            training_loss, eval_loss = loss
 
-    # show results on full image
-    with torch.no_grad():
-        loss_fn = torch.nn.L1Loss()
+            for j in range(len(models)):
+                csv_row = f"{csv_row},\t{training_loss[j]}"
+                console_row = f"{console_row},\t{training_loss[j]} ({type(models[j]).__name__})"
 
-        dataset.switch_mode(training=False, fullres=True)
-
-        model.eval()
-
-        for seq_idx, data in enumerate(dataloader):
-            print(f"Processing test sequence {seq_idx}")
-
-            seq_in, seq_ref = unsqueeze_inputs(data)
-
-            seq_in = seq_in.to(device)
-            seq_ref = seq_ref.to(device)
-
-            seq_out = model(seq_in)
-
-            loss = loss_fn(seq_out, seq_ref)
+            for j in range(len(models)):
+                csv_row = f"{csv_row},\t{eval_loss[j]}"
+                console_row = f"{console_row},\t{eval_loss[j]} ({type(models[j]).__name__})"
             
-            print(f"\tTotal loss on test sequence {seq_idx} was {loss.item()}\n\n")
-
-            dump_path = f"{os.environ['RTDENOISE_OUTPUT_PATH']}/test/seq{seq_idx}/"
-            os.makedirs(dump_path, exist_ok=True)
-
-            with open(dump_path + "loss.csv", "w") as f:
-                f.write("frameidx, loss\n")
-                f.write(f"-1, {loss.item()}\n")
-
-                for i in range(0, seq_out.shape[1], 3):
-                    still_frame = seq_out[0, i:i+3, :, :]
-                    still_loss = loss_fn(still_frame, seq_ref[0, i:i+3, :, :])
-
-                    f.write(f"{i // 3}, {still_loss.item()}\n")
-
-                    exr.imwrite(dump_path + f"frame{i // 3}.exr", still_frame.permute(1, 2, 0).cpu().numpy())
-
+            print(console_row)
+            f.write(f"{csv_row}\n")
             
 
     print("Done!")
--- src/rtdenoise/training/train.py
+++ src/rtdenoise/training/train.py
@@ -5,6 +5,9 @@ from . import prebatched_dataset
 import torch
 from torch.utils.data import DataLoader
 
+import os
+import openexr_numpy as exr
+
 def unsqueeze_inputs(data):
     inputs, reference = data
 
@@ -17,8 +20,18 @@ def unsqueeze_inputs(data):
 
     return inputs, reference
 
-def train_model(dataset : prebatched_dataset.PrebatchedDataset, dataloader : torch.utils.data.DataLoader, model: BaseDenoiser, optimizer : torch.optim.Optimizer, scheduler, num_epochs, device):
-    # placeholder
+def train_model(
+        dataset : prebatched_dataset.PrebatchedDataset,
+        dataloader : torch.utils.data.DataLoader, 
+        models : list[BaseDenoiser], 
+        optimizers : list[torch.optim.Optimizer], 
+        schedulers, 
+        names : list[str],
+        num_epochs, 
+        device
+    ):
+    num_models = len(models)
+
     loss_fn = torch.nn.L1Loss()
     losses = []
 
@@ -26,12 +39,15 @@ def train_model(dataset : prebatched_dataset.PrebatchedDataset, dataloader : tor
         print(f"Processing epoch {epoch}")
 
         # Training code
-        total_loss = 0.0
-        num_batches = 0
+        accum_training_loss = [0.0] * num_models
+        num_training_samples = 0
+
 
         dataset.switch_mode(training=True, fullres=False)
 
-        model.train()
+        for model in models:
+            model.train()
+        
         for batch_idx, data in enumerate(dataloader):
             print(f"\tProcessing training batch {batch_idx}")
 
@@ -39,30 +55,34 @@ def train_model(dataset : prebatched_dataset.PrebatchedDataset, dataloader : tor
             seq_in = seq_in.to(device)
             seq_ref = seq_ref.to(device)
 
-            optimizer.zero_grad()
+            for i in range(num_models):
+                optimizers[i].zero_grad()
 
-            seq_out = model(seq_in)
+                seq_out = models[i](seq_in)
 
-            loss = loss_fn(seq_out, seq_ref)
-            loss.backward()
+                loss = loss_fn(seq_out, seq_ref)
+                loss.backward()
 
-            optimizer.step()
+                optimizers[i].step()
 
-            total_loss += loss.item() *  seq_ref.shape[0]
-            num_batches  += seq_ref.shape[0]
+                print(f"\t\tLoss for model {names[i]}\twas {loss.item()}")
 
-        epoch_training_loss = total_loss / num_batches
+                accum_training_loss[i] += loss.item() * seq_in.shape[0]
+                
+            num_training_samples += seq_in.shape[0]
 
-        scheduler.step()
+        for scheduler in schedulers:
+            scheduler.step()
 
         with torch.no_grad():
 
-            total_loss = 0.0
-            num_batches = 0
+            accum_test_loss = [0.0] * num_models
+            num_eval_samples = 0
 
             dataset.switch_mode(training=False, fullres=False)
 
-            model.eval()
+            for model in models:
+                model.eval()
             for batch_idx, data in enumerate(dataloader):
                 print(f"\tProcessing eval batch {batch_idx}")
 
@@ -70,18 +90,55 @@ def train_model(dataset : prebatched_dataset.PrebatchedDataset, dataloader : tor
                 seq_in = seq_in.to(device)
                 seq_ref = seq_ref.to(device)
 
-                seq_out = model(seq_in)
+                for i in range(num_models):
+                    seq_out = models[i](seq_in)
 
-                loss = loss_fn(seq_out, seq_ref)
+                    loss = loss_fn(seq_out, seq_ref)
                 
-                # Update statistics
-                total_loss += loss.item() *  seq_ref.shape[0]
-                num_batches += seq_ref.shape[0]
+                    # Update statistics
+                    accum_test_loss[i] += loss.item() *  seq_ref.shape[0]
+                    num_eval_samples += seq_ref.shape[0]
 
-            epoch_loss = total_loss / num_batches
-            print(f"Training and epoch loss in epoch {epoch} was {epoch_training_loss}\t{epoch_loss}\n\n")
+            training_loss = [loss / num_training_samples for loss in accum_training_loss]
+            test_loss = [loss / num_training_samples for loss in accum_test_loss]
 
-            losses.append((epoch_training_loss, epoch_loss))
+            print(f"Training and epoch loss in epoch {epoch}:")
+            for i in range(num_models):
+                print(f"\t{names[i]}\t{training_loss[i]}\t{test_loss[i]}")
+
+            losses.append((training_loss, test_loss))
+
+            dataset.switch_mode(training=False, fullres=True)
+
+            for seq_idx, data in enumerate(dataloader):
+                print(f"Processing test sequence {seq_idx}")
+
+                seq_in, seq_ref = unsqueeze_inputs(data)
+
+                seq_in = seq_in.to(device)
+                seq_ref = seq_ref.to(device)
+
+                for i in range(num_models):
+                    seq_out = models[i](seq_in)
+
+                    loss = loss_fn(seq_out, seq_ref)
+                
+                    print(f"\tTotal loss on test sequence {seq_idx} was {loss.item()}\n\n")
+
+                    dump_path = f"{os.environ['RTDENOISE_OUTPUT_PATH']}/test/epoch{epoch}/seq{seq_idx}/"
+                    os.makedirs(dump_path, exist_ok=True)
+
+                    with open(dump_path + f"loss-{names[i]}.csv", "w") as f:
+                        f.write("frameidx, loss\n")
+                        f.write(f"-1, {loss.item()}\n")
+
+                        for j in range(0, seq_out.shape[1], 3):
+                            still_frame = seq_out[0, j:j+3, :, :]
+                            still_loss = loss_fn(still_frame, seq_ref[0, j:j+3, :, :])
+
+                            f.write(f"{j // 3}, {still_loss.item()}\n")
+
+                            exr.imwrite(dump_path + f"{names[i]}{j // 3}.exr", still_frame.permute(1, 2, 0).cpu().numpy())
 
     return (model, losses)
             
