0ccb06830c6ab68030b5857b00d4886f7116ab91
Saad Amin <58188800+saadam1n@users.noreply.github.com>
2025-03-25 19:29:36 -0400

    More thorough testing for multiple KV sources

--- tests/test_kernel_attn.py
+++ tests/test_kernel_attn.py
@@ -4,28 +4,33 @@ import rtdenoise.kernels as K
 import torch
 import torch.nn.functional as F
 
-qk = torch.randn(8, 24, 32, 32, requires_grad=True)
-v = torch.randn(8, 3, 32, 32, requires_grad=True)
+qklist = [torch.randn(8, 24, 32, 32, requires_grad=True) for _ in range(3)]
+vlist = [torch.randn(8, 3, 32, 32, requires_grad=True) for _ in range(3)]
 
-qkr = qk.detach()
-vr = v.detach()
-
-qkr.requires_grad_()
-vr.requires_grad_()
+qkrlist = [qk.detach().requires_grad_() for qk in qklist]
+vrlist = [v.detach().requires_grad_() for v in vlist]
 
 window_size = 3
 
-q_f = F.unfold(qkr, kernel_size=1, stride=1).unflatten(1, (-1, 1)).permute(0, 3, 2, 1)
-k_f = F.unfold(qkr, kernel_size=window_size, stride=1, padding=window_size // 2).unflatten(1, (-1, window_size ** 2)).permute(0, 3, 2, 1)
-v_f = F.unfold(vr, kernel_size=window_size, stride=1, padding=window_size // 2).unflatten(1, (-1, window_size ** 2)).permute(0, 3, 2, 1)
+q_f = F.unfold(qkrlist[0], kernel_size=1, stride=1).unflatten(1, (-1, 1)).permute(0, 3, 2, 1)
 
-k_f = torch.cat([k_f] * 3, dim=2)
-v_f = torch.cat([v_f] * 3, dim=2)
+
+
+k_f = torch.cat(
+    [F.unfold(qk, kernel_size=window_size, stride=1, padding=window_size // 2).unflatten(1, (-1, window_size ** 2)).permute(0, 3, 2, 1) for qk in qkrlist], 
+    dim=2
+)
+
+
+v_f = torch.cat(
+    [F.unfold(v, kernel_size=window_size, stride=1, padding=window_size // 2).unflatten(1, (-1, window_size ** 2)).permute(0, 3, 2, 1) for v in vrlist], 
+    dim=2
+)
 
 a_ref = F.scaled_dot_product_attention(q_f, k_f, v_f).permute(0, 3, 2, 1).flatten(1, 2)
-a_ref = F.fold(a_ref, output_size=v.shape[2:], kernel_size=1, stride=1)
+a_ref = F.fold(a_ref, output_size=vlist[0].shape[2:], kernel_size=1, stride=1)
 
-a_out = K.kernel_attn(qk, v, qk, v, qk, v, window_size)
+a_out = K.kernel_attn(qklist[0], vlist[0], qklist[1], vlist[1], qklist[2], vlist[2], window_size)
 
 if torch.isnan(a_ref).any():
     print("A Ref has NaN!")
@@ -40,8 +45,10 @@ print("\n\nBackwards tests")
 a_ref.sum().backward()
 a_out.sum().backward()
 
-errqk = F.l1_loss(qk.grad, qkr.grad)
-print(f"QK L1 loss was {errqk}")
 
-errv = F.l1_loss(v.grad, vr.grad)
-print(f"V L1 loss was {errv}")
+for i in range(3):
+    errqk = F.l1_loss(qklist[i].grad, qkrlist[i].grad)
+    print(f"QK L1 loss was {errqk}")
+
+    errv = F.l1_loss(vlist[i].grad, vrlist[i].grad)
+    print(f"V L1 loss was {errv}")
